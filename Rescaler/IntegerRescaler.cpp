0f548cbb2c359ab07bb9d16b24d5c3e72391c22b
The-Arkitekt <mr.davidcrumley@gmail.com>
2024-08-18 19:48:44 +0000

    added another check for outputmax

--- Rescaler/IntegerRescaler.cpp
+++ Rescaler/IntegerRescaler.cpp
@@ -9,9 +9,11 @@ IntegerRescaler::IntegerRescaler(int inputMin, int inputMax, int outputMin, int 
   outputMax  (0),
   scaleFactor(0)
 {
+  // make sure that outputMax << PRECISION is not greater than max int
   // make sure that outputRange << PRECISION is not greater than max int
   // make sure that inputRange is not zero
-  if (((outputMax - outputMin) > (std::numeric_limits<int>::max() >> PRECISION)) ||
+  if ((outputMax > std::numeric_limits<int>::max() >> PRECISION)                ||
+     ((outputMax - outputMin) > (std::numeric_limits<int>::max() >> PRECISION)) ||
       ((inputMax - inputMin) <= 0))
   {
     return;
@@ -31,7 +33,7 @@ IntegerRescaler::~IntegerRescaler()
 
 int IntegerRescaler::scale(int input)
 {
-  int output = scaleFactor * (input - inputMax) + outputMax;
+  int output = (scaleFactor * (input - inputMax)) + outputMax;
   return output >> PRECISION;
 }
 
